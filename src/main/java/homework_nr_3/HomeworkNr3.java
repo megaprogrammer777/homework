a59dd16414d6fa1e7600526a9a3f56df0da1ea2f
megaprogrammer777 <maksimtiuliu53@gmail.com>
2023-11-01 22:34:18 +0200

    Initial commit

---
+++ src/main/java/homework_nr_3/HomeworkNr3.java
@@ -0,0 +1,40 @@
+package homework_nr_2;
+
+public class HomeworkNr2{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        long CurrentWorldPopulation = 72123235 ;
+        long CarsProducedThisYear = 53322358;
+        char FirstLetterInAlphabet = 'A';
+        double Pi = 3.14159;
+        boolean JavaIsAmazing = true;
+
+
+
+        System.out.println(CurrentWorldPopulation);
+        System.out.println(CarsProducedThisYear);
+        System.out.println(FirstLetterInAlphabet);
+        System.out.println(Pi);
+        System.out.println(JavaIsAmazing);
+
+
+        double Price = 456548.34;
+        double Tax = 23543.43;
+        int Quantity = 53;
+        double Sum = 0;
+        int Total = (int) (Price + Tax * Quantity + Sum) ;
+        System.out.println(Price);
+        System.out.println(Tax);
+        System.out.println(Quantity);
+        System.out.println(Sum);
+        System.out.println(Total);
+
+
+
+
+
+
+
+    }
+}
