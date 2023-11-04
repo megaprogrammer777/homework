00784fe6befb464469eff45cc530c8b22fbd5f69
megaprogrammer777 <maksimtiuliu53@gmail.com>
2023-11-04 17:52:30 +0200

    Initial commit I search March in one eyar.

---
+++ src/main/java/homework_nr_5/HomeworkNr5.java
@@ -0,0 +1,48 @@
+package homework_nr_5;
+
+public class HomeworkNr5 {
+    public static void main(String[] args) {
+        int month = 3;
+        switch (month){
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("Jule");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("Septembr");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("Decembr");
+                break;
+            default:
+                System.out.println("invalid month");
+
+        }
+    }
+}
