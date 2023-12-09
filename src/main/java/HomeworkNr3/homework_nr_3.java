4b0ad4bb2027e88ccfcdd5534a3fa1723999a174
megaprogrammer777 <maksimtiuliu53@gmail.com>
2023-12-09 21:18:34 +0200

    Sum of series 1/3 to 97/99

---
+++ src/main/java/HomeworkNr3/homework_nr_3.java
@@ -0,0 +1,12 @@
+package HomeworkNr3;
+
+public class homework_nr_3 {
+    public static void main(String[] args) {
+
+        double sum = 0.0;
+        for (int i = 1, j = 3; i <= 97 && j <= 99; i += 2, j += 2){
+            sum += (double) i / j;}
+        System.out.println("Сумма серий" + sum);
+    }
+}
+
