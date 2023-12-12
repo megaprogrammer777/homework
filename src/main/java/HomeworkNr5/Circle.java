ccc0ce5bf8f29d80fc505526b6d8418ab4e71b6e
megaprogrammer777 <maksimtiuliu53@gmail.com>
2023-12-12 17:43:18 +0200

    Create object from class Circle

--- src/main/java/HomeworkNr5/Circle.java
+++ src/main/java/HomeworkNr5/Circle.java
@@ -3,17 +3,19 @@ package HomeworkNr5;
 public class Circle {
     public static void main(String[] args) {
         double radius = 23.5;
-        calculateArea(radius);
-
-
 
     }
 
-    public static void calculateArea(double radius){
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public  void calculateArea(double radius){
         double area = Math.PI * Math.pow(radius,2);
         System.out.println("Площадь круга" + area);
 
-
-
     }
+
 }
---
+++ src/main/java/HomeworkNr5/Main.java
@@ -0,0 +1,9 @@
+package HomeworkNr5;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle Ball = new Circle(45);
+        Ball.calculateArea(Ball.radius);
+    }
+
+}
