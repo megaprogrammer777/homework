1d9f683acfeeed2af66dd6da54b3c4609e5696af
megaprogrammer777 <maksimtiuliu53@gmail.com>
2023-12-15 14:49:15 +0200

    Commit

--- src/main/java/HomeworkNr7/Massive.java
+++ src/main/java/HomeworkNr7/Massive.java
@@ -4,15 +4,23 @@ public class Massive {
     public static void main(String[] args) {
         int[]numbers = new int[100];
         for (int index = 0; index < numbers.length;index++){
-            numbers[index] = index + 1;
+            numbers[index] = index + 0;
         }
 
         int sum = 0;
-        for (int index = 0; index < numbers.length; index++) {
+        for (int index = 0; index < numbers.length; index +=2) {
             System.out.println(numbers[index]);
             sum += numbers[index];
         }
-
         System.out.println("Sum "+ sum);
+
+        System.out.println("--------------------------------------");
+
+        int sum2 = 1;
+        for(int index = 1;index < numbers.length;index +=2) {
+            System.out.println(numbers[index]);
+            sum2 = sum2 * numbers[index];
+        }
+        System.out.println("Sum of not even numbers is" + sum2);
     }
 }
